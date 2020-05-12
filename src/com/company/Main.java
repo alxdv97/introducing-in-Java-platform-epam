f8689d8c374a2e639aa715d927808c1f1e685a77
alxdv97 <alexey.deyev@gmail.com>
2020-05-12 11:01:14 +0300

    task for module "Introducing in Java platform" in epam

---
+++ src/com/company/Main.java
@@ -0,0 +1,33 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int i = 1;
+        System.out.println(i);
+        i = i + 1;
+        System.out.println(i);
+        i = i + 1;
+        System.out.println(i);
+        System.out.println("1.");
+        System.out.println("2..");
+        System.out.println("3...");
+        System.out.println("Hello World!");
+
+        int a = 1;
+        int b = 2;
+        int c = a + b;
+        System.out.println("sum = " + c);
+
+        int d = 1;
+        int e = 2;
+        System.out.println("d < e = " + (d < e));
+        System.out.println("d > e = " + (d > e));
+
+        String hello = "Hello";
+        String world = "world";
+        System.out.println(hello + " " + world + "!");
+
+
+    }
+}
